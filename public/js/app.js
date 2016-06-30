975b6e6e7a256314ad13a55e6a8110970a6e1874
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-06-30 08:31:58 +0300

    basic ajax: get weather from API and show in console.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 var weatherForm = document.getElementById("weather-form").onsubmit = getWeather;
 var resultsDiv = document.getElementById("results");
+const API_KEY = "974cf989f33a668473fd4d704c86c5e8";
 
 function getWeather() {
   var city = document.getElementById("input-city").value;
@@ -7,7 +8,7 @@ function getWeather() {
   var txtInfo = document.createTextNode(city + " - AJAX call...");
   paragraph.appendChild(txtInfo);
   resultsDiv.appendChild(paragraph);
-  
+  ajaxCall(city);
   return false;
 }
 
@@ -16,4 +17,20 @@ function clearResults() {
   while (resultsDiv.firstChild) {
     resultsDiv.removeChild(resultsDiv.firstChild);
   }
+}
+
+function ajaxCall(city) {
+
+  const API_URL = "http://api.openweathermap.org/data/2.5/";
+  var weatherType = "weather";
+  var URL = API_URL + weatherType + "?q=" + city + "&APPID=" + API_KEY;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      console.log(xhttp.responseText);
+    }
+  };
+  xhttp.open("GET", URL, true);
+  xhttp.send();
 }
