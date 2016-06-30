49673099ac1b96534cafa618147d7af1bc4b39f1
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-06-30 08:05:48 +0300

    add basic js code for display and clear results.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log("Weather client");
+var weatherForm = document.getElementById("weather-form").onsubmit = getWeather;
+var resultsDiv = document.getElementById("results");
+
+function getWeather() {
+  var city = document.getElementById("input-city").value;
+  var paragraph = document.createElement("p");
+  var txtInfo = document.createTextNode(city + " - AJAX call...");
+  paragraph.appendChild(txtInfo);
+  resultsDiv.appendChild(paragraph);
+  
+  return false;
+}
+
+var clearBtn = document.getElementById("clear-btn").onclick = clearResults;
+function clearResults() {
+  while (resultsDiv.firstChild) {
+    resultsDiv.removeChild(resultsDiv.firstChild);
+  }
+}
